ed24e93818eb434c749143f8cc72880cc16f387e
dkershner6 <dkershner@cbtnuggets.com>
2020-05-30 13:17:16 -0700

    1.0.8 - Made the interface way easier to understand and use

--- src/tasks/parseGitIgnore.ts
+++ src/tasks/parseGitIgnore.ts
@@ -1,17 +1,19 @@
-import { debug, setOutput } from '@actions/core';
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-ignore - No Types
-import parse from 'parse-gitignore';
+import parser from 'gitignore-parser';
 import fs from 'fs';
 import { join } from 'path';
 
-const parseGitIgnore = (path: string): Set<string> => {
-    const gitIgnoreLines: string[] = parse(
-        fs.readFileSync(join(path, '.gitignore'))
-    );
-    const gitIgnoreLinesAsString = gitIgnoreLines.join(',');
-    debug(gitIgnoreLinesAsString);
-    setOutput('gitignored', gitIgnoreLinesAsString);
-    return new Set(gitIgnoreLines);
+export interface IGitIgnore {
+    accepts: (filePathAndName: string) => boolean;
+    denies: (filePathAndName: string) => boolean;
+}
+
+const parseGitIgnore = (path: string): IGitIgnore => {
+    const gitIgnoreFile = fs.readFileSync(join(path, '.gitignore'), 'utf8');
+
+    const gitIgnore: IGitIgnore = parser.compile(gitIgnoreFile);
+
+    return gitIgnore;
 };
 export default parseGitIgnore;
